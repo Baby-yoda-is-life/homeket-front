5374ae6ac7cb3e522b821447e0f38b9b64299a24
Alejandro B <ablancodev@gmail.com>
2023-04-04 20:22:45 +0200

    Suggestions

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(import.meta.env.VITE_URL_SUPABASE, import.meta.env.VITE_API_KEY);
+export const dbInstance = createClient(import.meta.env.VITE_URL_SUPABASE, import.meta.env.VITE_API_KEY);
